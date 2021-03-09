1723d2f57a4a2f8460974c2e74b8bfd07334223c
Jay Butera <buterajay@protonmail.com>
2021-03-09 14:21:14 -0600

    Read code from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use mil::parser;
+use std::fs::File;
+use std::io::prelude::*;
 
-fn main() {
-    println!("{:?}", parser::parse_sexp("(x x x)"));
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("./examples/tmp.mil")?;
+    let mut code = String::new();
+    file.read_to_string(&mut code)?;
+
+    println!("{:?}", parser::parse_sexp(&code[..]));
+    Ok(())
 }
